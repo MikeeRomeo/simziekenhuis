25b9d9ad3f261480583c932e76979ec294eaa6ae
Mike <info@mikeromeo.nl>
2021-03-16 11:21:44 +0100

    forgot connect again

--- client/src/components/GameView.vue
+++ client/src/components/GameView.vue
@@ -74,8 +74,7 @@ export default {
         }
     },
     created() {
-        this.socket = io.connect("http://localhost:3000");
-        // this.socket = io.connect("https://mikeromeo.codeguys.nl:3000");
+        this.socket = io.connect("https://mikeromeo.codeguys.nl:3000");
         this.socket.emit('join', 'Game');
     },
     mounted() {
