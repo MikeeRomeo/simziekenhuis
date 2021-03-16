94f6da9de92656d845ddaf6c915edb37ca6a4ac6
Mike <info@mikeromeo.nl>
2021-03-16 09:38:44 +0100

    log stlying

--- client/src/components/game/user-tabs/ActionLog.vue
+++ client/src/components/game/user-tabs/ActionLog.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="action-log">
         <div class="message" v-for="log in sortedByUser">
-            {{ log }}
+            {{ log.message }}
+<!--            <span v-if="log.type === 'intervention'"> is toegepast</span>-->
         </div>
     </div>
 </template>
@@ -12,28 +13,37 @@ import {mapGetters} from 'vuex';
 
 export default {
     name: "ActionLog",
-    props:['selectedUser'],
+    props: ['selectedUser'],
     mounted() {
     },
-    computed:{
+    computed: {
         ...mapGetters(['userLogs']),
 
-        sortedByUser(){
+        sortedByUser() {
             return this.userLogs.filter(log => log.user === this.selectedUser);
         }
     },
-    components:{
+    components: {
         ResearchRequest,
     },
 }
 </script>
 
 <style lang="scss" scoped>
-    .action-log{
-        margin-top: 20px;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: space-between;
-    }
+.action-log {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.message {
+    font-size: 12px;
+    padding: 8px 15px;
+    font-weight: 600;
+    color: #2F4858;
+    border-radius: 50px;
+    background: white;
+    margin-bottom: 8px;
+    box-shadow: rgba(47, 72, 88, 8%) 0 2px 8px;
+}
 </style>
